9c0fc50872ca1791b616413c79e7a0a9580a3f00
Juan COYLA <jcoyla123@gmail.com>
2020-04-16 01:56:16 +0200

    chore: build review page with loyout component

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <header> this is header </header> 
+  )
+}
---
+++ src/components/layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './header'
+import Footer from './footer'
+
+export default ({ children }) => {
+  return (
+    <div>
+      <Header/>
+      <div> this is layout content: </div>
+      <div> { children } </div>
+      <Footer/>
+    </div>
+  )
+}
