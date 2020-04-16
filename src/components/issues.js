485adc61261d0a30984cf839d03cc961ec05d230
Juan COYLA <jcoyla123@gmail.com>
2020-04-17 00:30:50 +0200

    issues page

---
+++ src/components/issues.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Issues extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div> list of issues </div>
+    )
+  }
+}
+
+export default Issues;
--- src/pages/review.js
+++ src/pages/review.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Issues from '../components/issues';
 
 const Review = () => {
   return (
     <Layout>
-      <h1> Review page </h1>
+      <Issues/>
     </Layout>
   )
 }
