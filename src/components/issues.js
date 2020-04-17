0cf8c7d58b5cd116f52192b1ea8c585e1d7b0f53
Juan COYLA <jcoyla123@gmail.com>
2020-04-18 00:48:04 +0200

    chore: fetch data from API

--- src/components/issues.js
+++ src/components/issues.js
@@ -3,6 +3,8 @@ import React from 'react';
 class Issues extends React.Component {
   constructor(props) {
     super(props);
+    this.props = props;
+    console.log(this.props)
   }
 
   render() {
--- src/pages/review.js
+++ src/pages/review.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import Issues from '../components/issues';
 
 const Review = () => {
+
+  const [userName, setUserName] = useState(0);
+  useEffect(() => {
+    // get data from GitHub api
+    const test = async () => {
+      const data = await fetch(`https://api.github.com/repos/gatsbyjs/gatsby`)
+      const json = await data.json();
+      setUserName(json);
+    };
+    test();
+  }, [])
+
+
   return (
     <Layout>
-      <Issues/>
+      { `test: ${userName}`} 
+      <Issues git={userName}/>
     </Layout>
   )
 }
