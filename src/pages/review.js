fef441f3b799b89a0a7f05dc2a0978e199f3f110
Juan COYLA <jcoyla123@gmail.com>
2020-04-19 01:53:16 +0200

    chore: using fetch to connect graphql

--- src/pages/review.js
+++ src/pages/review.js
@@ -1,23 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import Issues from '../components/issues';
+import ApolloClient, { gql } from 'apollo-boost';
+import { execute } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http';
 import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
+
 
 
 const Review = () => {
 
-  const query = gql`
-  query {
+  const query = `{
+  
     viewer {
-      name
+    issues(
+      first: 5
+      orderBy: { field: UPDATED_AT, direction: ASC},
+      filterBy: { since: "2020-04-18T00:00:00Z"}
+    ) {
+        edges {
+          node {
+            repository {
+              name
+            }
+            labels(first: 4) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+            title,
+            createdAt,
+            updatedAt
+            state
+            comments(first:10) {
+              edges {
+                node {
+                  bodyHTML
+                  createdAt,
+                  updatedAt
+                }
+              }
+            }
+          }
+        }
+      }
     }
-  }
-`;
+  }`;
 
 
   const [userName, setUserName] = useState(0);
   useEffect(() => {
+
+    fetch('https://api.github.com/graphql', {
+    method: 'POST',
+   headers: {
+    'Accept': 'application/json',
+     'Content-Type': 'application/json',
+     'Authorization': 'Bearer 187ee7c14663be1e0b933595756151568264d7e2',
+    },
+  body: JSON.stringify({ query }),
+})
+  .then(res => {
+    console.log(res);
+    return res.json()
+  })
+  .then(res => console.log(res.data));
     // get data from GitHub api
   }, [])
 
