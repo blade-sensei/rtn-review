c5e64335949895e479198110949ad8bccacd8783
Juan COYLA <jcoyla123@gmail.com>
2020-04-18 23:42:06 +0200

    chore: setup client apollo to use graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
 import React from "react"
+import ApolloClient, { gql } from 'apollo-boost';
+
+const token = 'e2b30fa62a9daac7f09a08d4a637a2dbab917660';
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+  }
+});
+
+client
+  .query({
+    query: gql`
+    query {
+      viewer {
+        name
+      }
+    }    
+    `
+  })
+  .then(result => console.log(result.data));
 
 export default () => <div>Hello world!</div>
--- src/pages/review.js
+++ src/pages/review.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import Issues from '../components/issues';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
 
 const Review = () => {
+
+  const query = gql`
+  query {
+    viewer {
+      name
+    }
+  }
+`;
+
 
   const [userName, setUserName] = useState(0);
   useEffect(() => {
     // get data from GitHub api
-    const test = async () => {
-      const data = await fetch(`https://api.github.com/user/blade-sensei/issues`, {
-        crossDomain: true,
-      })
-      const json = await data.json();
-      console.log(json);
-      setUserName(json);
-    };
-    test();
   }, [])
 
 
